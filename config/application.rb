6d93c74754b041e8fe834150a46040b3789b20bb
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-03-21 15:29:54 +0200

    Loads locale files in nested dirs

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Finnishcities
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # loads locale files in nested directories
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
