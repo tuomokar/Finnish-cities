5033aa468acc756ccb850751ae8c2122d90f6f7a
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-03-26 13:00:41 +0200

    capybara tests for Finnish locale

--- spec/features/regions_page_spec.rb
+++ spec/features/regions_page_spec.rb
@@ -1,40 +1,79 @@
 require 'rails_helper'
 
 describe "Regions page" do
-  it "shows the 'Regions' text" do
-    visit regions_path
-    expect(page).to have_content "Regions"
-  end
+  describe "in English" do
 
-  describe "having existing regions" do
-    let(:regions){["Southwest Finland", "North Karelia", "Northern Savonia"]}
-
-    it "lists each regions' name" do
-      regions.each do |region_name|
-        FactoryGirl.create(:region_en, name:region_name)
-      end
-
+    it "shows the 'Regions' text" do
       visit regions_path
-
-      regions.each do |region_name|
-        expect(page).to have_content region_name
-      end
+      expect(page).to have_content "Regions"
     end
 
-    it "allows the user to navigate to page of a region" do
-      regions.each do |region_name|
-        FactoryGirl.create(:region_en, name: region_name)
+    describe "having existing regions" do
+      let(:regions){["Southwest Finland", "North Karelia", "Northern Savonia"]}
+
+      it "lists each regions' name" do
+        regions.each do |region_name|
+          FactoryGirl.create(:region_en, name:region_name)
+        end
+
+        visit regions_path
+
+        regions.each do |region_name|
+          expect(page).to have_content region_name
+        end
       end
 
-      visit regions_path
+      it "allows the user to navigate to page of a region" do
+        regions.each do |region_name|
+          FactoryGirl.create(:region_en, name: region_name)
+        end
 
-      click_link "North Karelia"
+        visit regions_path
 
-      expect(page).to have_content("North Karelia")
-      expect(page).to have_content("Area: 1000.0")
+        click_link "North Karelia"
+
+        expect(page).to have_content("North Karelia")
+        expect(page).to have_content("Area: 1000.0")
+      end
     end
-
   end
 
+  describe "in Finnish" do
+    it "shows the 'Maakunnat' text" do
+
+      visit regions_path(:fi)
+      expect(page).to have_content "Maakunnat"
+    end
+
+    describe "having existing regions" do
+
+      let(:regions){["Varsinais-Suomi", "Pohjois-Karjala", "Pohjois-Savo"]}
+
+      it "lists each regions' name" do
+        regions.each do |region_name|
+          FactoryGirl.create(:region_en, name:region_name)
+        end
+
+        visit regions_path(:fi)
+
+        regions.each do |region_name|
+          expect(page).to have_content region_name
+        end
+      end
+
+      it "allows the user to navigate to page of a region" do
+        regions.each do |region_name|
+          FactoryGirl.create(:region_en, name: region_name)
+        end
+
+        visit regions_path(:fi)
+
+        click_link "Pohjois-Karjala"
+
+        expect(page).to have_content("Pohjois-Karjala")
+        expect(page).to have_content("Pinta-ala: 1000.0")
+      end
+    end
+  end
 
 end
