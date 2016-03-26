729ae67582c146c13fa231f44b1e4b597fd1fed1
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-03-26 17:38:06 +0200

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326110239) do
+ActiveRecord::Schema.define(version: 20160326145907) do
+
+  create_table "municipalities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "year"
+    t.text     "description"
+    t.integer  "population"
+    t.string   "link"
+    t.integer  "region_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "region_translations", force: :cascade do |t|
     t.integer  "region_id",   null: false
