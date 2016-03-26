e3c063569229a33d3526324d6ffd63edafa9b481
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-03-26 21:01:51 +0200

    has_many and belongs_to

--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -1,3 +1,5 @@
 class Municipality < ActiveRecord::Base
   translates :name, :link, :description,:fallbacks_for_empty_translations => true
+
+  belongs_to :region
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,3 +1,5 @@
 class Region < ActiveRecord::Base
   translates :name, :description, :link, :fallbacks_for_empty_translations => true
+
+  has_many :municipalities
 end
