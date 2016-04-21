8fa5802eebf1556d6a9cf9326b05130a348ddde9
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-04-21 11:02:41 +0300

    restaurant place

---
+++ app/models/restaurant.rb
@@ -0,0 +1,3 @@
+class Restaurant < ActiveRecord::Base
+  acts_as :place
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419180750) do
+ActiveRecord::Schema.define(version: 20160419192842) do
 
   create_table "municipalities", force: :cascade do |t|
     t.string   "name"
@@ -81,6 +81,11 @@ ActiveRecord::Schema.define(version: 20160419180750) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
