c9404c0cfeede34b612af37779628004414f3283
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-05-14 18:18:57 +0300

    seeing if this fixes the dependency resolving problem with minifying

--- app/assets/javascripts/angular/angularApp.js
+++ app/assets/javascripts/angular/angularApp.js
@@ -1,8 +1,8 @@
 var App = angular.module('FinnishMunicipalities', ['ngRoute', 'ngTurbolinks']);
 
-App.config(function($locationProvider) {
+App.config(['$locationProvider', function($locationProvider) {
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
     });
-});
+}]);
