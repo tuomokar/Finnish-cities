323cc867b410ee66100daa0d93988783faf1b0aa
Tuomo Oila <tuomo.a.karkkainen@helsinki.fi>
2016-05-14 13:02:53 +0300

    seeing if this fixes a problem with heroku not understanding  config

--- app/assets/javascripts/angular/angularApp.js
+++ app/assets/javascripts/angular/angularApp.js
@@ -1,8 +1,12 @@
 var App = angular.module('FinnishMunicipalities', []);
 
-App.config(function($locationProvider) {
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
+App.$inject = ['$locationProvider'];
+App.config(
+
+    function useLocationInHtml5Mode($locationProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
 });
